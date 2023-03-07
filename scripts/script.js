a65ec62d8a29c25179e916579c572e588bdf8e49
vishnu <trainee@gmail.com>
2023-03-07 17:44:23 +0530

    assessment completed:

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,25 @@
 $(document).ready(function(){
+	jQuery.validator.addMethod("lettersonly", function(value, element) 
+	{
+return this.optional(element) || /^[a-z," ",A-Z]+$/i.test(value);
+}, "Letters and spaces only please"); 
 	$("#student_registration_form").validate({
 		rules:{
 			first_name:{
+				lettersonly:true,
 				required:true,
 				maxlength:120,
 			},
 			last_name:{
 				maxlength:120,
+				number:false,
 			},
 			age:{
 				required:true,
 				number:true,
 			},
 			register_number:{
+				number:true,
 				required:true,
 				maxlength:12,
 				minlength:12,
@@ -34,14 +41,38 @@ $(document).ready(function(){
 			address:{
 				required:true,
 
+			},
+			pincode:{
+				required:true,
+				maxlength:6,
+				minlength:6,
+				number:true
+			},
+			family_income:{
+				number:true,
+			},
+			radio:{
+				required:true,
+			},
+			height:{
+				number:true,
+				maxlength:3,
+			},
+			weight:{
+				number:true,
+				maxlength:3,
+			},
+			agree_terms:{
+				required:true,
 			}
+
 
 
 		},
 		messages:{
 			first_name:{
 				required:"Please Enter Your First Name",
-				maxlength:120,
+				maxlength:"Maxlength exceededs",
 			},
 			last_name:{
 				maxlength:"Character limit exceded",
@@ -68,11 +99,63 @@ $(document).ready(function(){
 			program:{
 				required:"Select any one Program",
 
-			}
+			},
 			address:{
-				required:"Please Enter Your Address"
+				required:"Please Enter Your Address",
+			},
+			pincode:{
+				required:"Please Enter your Pincode",
+				maxlength:"6 digits required",
+				minlength:"6 digits required",
+				number:"Enter a valid pincode"
+			},
+			family_income:{
+				number:"Enter a valid amount",
+			},
+			height:{
+				number:"Enter a valid Digit",
+				maxlength:"invalid height value",
+			},
+			weight:{
+				number:"Enter a valid Digit",
+				maxlength:"invalid weight value",
+			},
+			agree_terms:{
+				required:"",
 			}
 
-		}
+
+		},
+		// function to place the error label  element after the main label
+		// function helps in ui layout error
+		errorPlacement: function(error, element) 
+        {
+            if ( element.is(":radio" || ":checkbox") ) 
+            {
+                error.appendTo(element.parents('#gender') )
+                error.appendTo(element.parents('#terms') );
+            }
+            else 
+            { // This is the default behavior
+                error.insertAfter( element );
+            }
+        },
+
+        
 	})
+
+// function to change button color 
+
+
+$('input:checkbox').change(function(){
+    if($(this).is(":checked")) { // if the agree terms checkbox is checked color is blue
+        $('#submit_button').addClass("btn-primary");
+        $('#submit_button').removeClass("btn-secondary");
+    } else { // else the color is gray
+        $('#submit_button').addClass("btn-secondary");
+        $('#submit_button').removeClass("btn-primary");
+    }
+})
+
+
 })
